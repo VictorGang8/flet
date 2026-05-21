836eb00a2cff1d3e37037670d3f58ad4f01fe878
VictorGang8 <victor.vgtogabriel@gmail.com>
2026-05-20 21:50:44 -0300

    Primeiro commit

---
+++ index.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main(page:ft.Page):
+    def clicar(e):
+        texto.value = "App Flet"
+        page.update()
+        
+    texto = ft.Text("Usando a Biblioteca Flet")
+    botao = ft.ElevatedButton("Clique Aqui", on_click=clicar)
+    
+    page.add(texto, botao)
+
+ft.app(target=main)
